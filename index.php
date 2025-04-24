0d957492571da78e07acda6db4c252b29ee652e0
Chandra-Sekhar-Das <147060644+Chandra-Sekhar-Das@users.noreply.github.com>
2025-04-24 09:53:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <html>
 <head>
-    <title>ELECTRICITY BILL COST CALCULATOR</title>
-	<link rel="stylesheet" href="style.css">
+    <title>ELECTRIC BILL CALCULATOR</title>
 </head>
 <body>
-    <div class="form-container">
-        <h2>ELECTRICITY BILL COST CALCULATOR</h2>
+        <h1>ELECTRICITY BILL COST CALCULATOR</h1>
         <form action="process.php" method="POST">
             <h3>Enter Units Consumed:</h3>
             <input type="Number" name="txtunits" required><br><br>
 
             <input type="submit" value="Submit">
         </form>
-    </div>
 </body>
 </html>
